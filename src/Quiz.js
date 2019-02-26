f7d6794da0ebc87c62f4dc5db2277388ea38ec42
Sergio Bachiller <s.bachillerrubia@gmail.com>
2019-02-26 13:24:28 +0100

    Module 1

---
+++ src/Quiz.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+
+let quizData = require("./quiz_data.json");
+
+class Quiz extends Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return (
+            <div><div className="QuizQuestrion">{quizData.quiz_questions[0].instruction_text}</div></div>
+            )
+    }
+
+
+}
+
+
+export default Quiz;
